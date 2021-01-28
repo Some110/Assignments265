0bac8b0d2f223ac24bbfadf0cf3ec3f83219471c
Soumyadip Majumder <somdip94@gmail.com>
2021-01-28 18:02:45 +0530

    erors in check_pallindrom.sh solved

--- Day6/check_pallindrom.sh
+++ Day6/check_pallindrom.sh
@@ -5,26 +5,41 @@ read -p " enter any two numbers to check if they are pallindroms " r1  r2
 
 function check_pallindrom() {
 
-	$1=num
+	
+
+	num=$1
 	k=$num
-	local rev
-	while [	$num -gt 0 ];
+
+	local rev=0
+	
+	while [	$k -gt 0 ];
 	do
-		k=$(( $k % 10 ))
-		rev=$(($rev + $k))
+
+		u=$(($k % 10))
+		rev=$(($rev + $u))
 		rev=$(($rev * 10))
 		
-		k=$((k/10))
+		k=$(($k/10))
 
 	done
+	
+	rev=$(($rev/10))
+
+	
 
 	if [ $rev -eq $num ]; then 
 
 		echo "$num is pallindrom "
+	else
+
+
+		echo " $num is not pallindrom "
+
+
 	fi
 }
 
 
 
-check_pallindrom($r1)
-check_pallindrom($r2)
+check_pallindrom $r1
+check_pallindrom $r2
